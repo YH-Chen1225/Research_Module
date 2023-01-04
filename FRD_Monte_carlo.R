bb88db1bd16550b5a8650e513515b6a36c143970
YH.C <ua89622u@gmail.com>
2023-01-04 20:13:47 +0100

    adding_the_parametric_method

--- FRD_Monte_carlo.R
+++ FRD_Monte_carlo.R
@@ -5,6 +5,7 @@ library(rdd)
 library(rddtools)
 library(rddensity)
 library(rdrobust)
+library(MuMIn)
 
 x<-runif(100,-2,2)
 ran<- sample(0:100, 10, TRUE)
@@ -82,8 +83,32 @@ return(mean_kernel)
 }
 
 #DGP can be DGF or DGF2, which is for non-linear simulation
-m <- FRD(40,5,DGP=DGF,kernel=kernel,n=500)
+m <- FRD(100,4,DGP=DGF2,kernel=kernel,n=500)#n cannot be too small
 m
+
+###Parametric Method
+vars <- c("w_s","data$x_centered","I(x_centered^2)","I(x_centered^3)","I(x_centered^4)")
+
+para <- function(T,vars,c,DGP){
+  coe <- matrix(NA,100,4)
+  for (i in 1:T){
+    a[i]<-runif(1,min=1,max=20)
+    data<-DGP(n,a[i])
+    data$x_centered<-data$x-c
+    for (z in 1:length(vars)-1){
+      formula <- as.formula(paste("data$y",paste(vars[1:z+1],collapse = "+"),sep = "~"))
+      fit <- lm(formula,data=data)
+      coe[i,z] <- fit$coefficients[1]
+    }
+  }
+  avg <- apply(coe, 2, mean)
+  return(avg)
+}
+
+##DGP can be DGF or DGF2, which is for non-linear data generating process
+avg <- para(100,vars=vars,c=0,DGP=DGF2)
+avg
+
 
 
 
